8960e27b46d57a6996bffa6b5030a30d20037cbe
Mohammad Atwi <mega.web.own@gmail.com>
2016-08-06 06:02:50 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,649 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+   <title>Simple Apk</title>
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+<link rel='shortcut icon' type='image/png' href='favicon.png'/>
+<meta charset="UTF-8">
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<meta charset="ISO-8859-1"> 
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+<link href="twitter-bootstrap-dev/docs/assets/css/bootstrap.css" rel="stylesheet" media="screen">
+<link href="twitter-bootstrap-dev/docs/assets/css/docs.css" rel="stylesheet" media="screen">
+<link href="twitter-bootstrap-dev/docs/assets/css/bootstrap-responsive.css" rel="stylesheet" media="screen">
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body style="padding-right: 0px;">
+<div id="bann" ><img id="bann" style="width: 58px;" src="droid.png"><b>Created By  <a id="fbb" href="http://facebook.com/martin.atwi.85"> Mohammad Atwi </a></b></div>
+<div id="credit">Simple APK<br><b id="tt">You don't need a store</b>
+</div>
+<div id="lang"><img src="lang.png" id="ln"><a id="eng" href="index.php">EN</a><a href="index-ar.php"><b id="ar">AR</b></a></div>
+	<!-- start APK Hotel -->
+	<div id="sitee">
+<div id="btt"><button data-target="#myModal" data-toggle="modal" class="btn btn-default" type="button" id="mainbt">APK Hotel</button></div>
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">APK Hotel<b id="lit"> (www.apkhotel.com)</b></h4>
+		<hr>
+		<table class="table table-responsive">
+	<thead>
+		<tr>
+			<th>
+				Ads
+			</th>
+			<th>
+				Captcha
+			</th>
+			<th>
+				Steps
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+				Few
+			</td>
+			<td>
+				No
+			</td>
+			<td>
+				1
+			</td>
+		</tr>
+	</tbody>
+</table>
+<!-- Rank -->
+
+  <button type="button" class="btn btn-info" id="bttn" data-toggle="collapse" data-target="#demo1">Alexa Rank</button>
+  <div id="demo1" class="collapse">
+    <div id="rank"><a href="http://www.alexa.com/siteinfo/apkhotel.com"><script type="text/javascript" src="http://xslt.alexa.com/site_stats/js/t/a?url=apkhotel.com"></script></a></div>
+  </div>
+
+<!-- Rank -->
+      </div>
+      <div class="modal-body">
+      	<?php  
+    $action_path = "http://www.apkhotel.com/search/app/";
+    ?>
+	    <script type="text/javascript">
+        function submitData() {
+            var a = null;
+            var _A = document.getElementById("_A").value;
+
+            a = _A;
+
+            window.location.href = "<?php echo $action_path; ?>?search_text=" + a;
+
+            return;
+        }
+    </script>
+    <h4 id="tiitle">e.g. whatsapp</h4>
+<input type="text" id="_A" class="form-control input-lg" placeholder="app name" name="apkhotel" required>
+<input type="submit" onclick="submitData();" value="Search" class="btn btn-default" id="btt2" name="apkhotel">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<!-- end -->
+
+<!-- start APK20 -->
+	<div id="sitee">
+<div id="btt"><button data-target="#myModal2" data-toggle="modal" class="btn btn-default" type="button" id="mainbt">APK 20</button></div>
+<div id="myModal2" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">APK 20<b id="lit"> (www.apk20.com)</b></h4>
+						<hr>
+		<table class="table table-responsive">
+	<thead>
+		<tr>
+			<th>
+				Ads
+			</th>
+			<th>
+				Captcha
+			</th>
+			<th>
+				Steps
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+				Few
+			</td>
+			<td>
+				No
+			</td>
+			<td>
+				1
+			</td>
+		</tr>
+	</tbody>
+</table>
+<!-- Rank -->
+
+  <button type="button" class="btn btn-info" id="bttn" data-toggle="collapse" data-target="#demo2">Alexa Rank</button>
+  <div id="demo2" class="collapse">
+    <div id="rank"><a href="http://www.alexa.com/siteinfo/apk20.com"><script type="text/javascript" src="http://xslt.alexa.com/site_stats/js/t/a?url=apk20.com"></script></a></div>
+  </div>
+
+<!-- Rank -->
+      </div>
+      <div class="modal-body">
+      	<?php  
+    $action_path2 = "http://www.apk20.com/search/app/";
+    ?>
+	    <script type="text/javascript">
+        function submitData2() {
+            var a = null;
+            var _B = document.getElementById("_B").value;
+
+            a = _B;
+
+            window.location.href = "<?php echo $action_path2; ?>?search_text=" + a;
+
+            return;
+        }
+    </script>
+    <h4 id="tiitle">e.g. whatsapp</h4>
+<input type="text" name="apk20" id="_B" class="form-control input-lg" placeholder="app name" required>
+<input type="submit" name="apk20" onclick="submitData2();" value="Search" class="btn btn-default" id="btt2">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<!-- end -->
+
+<!-- start APK Here -->
+	<div id="sitee">
+<div id="btt"><button data-target="#myModal3" data-toggle="modal" class="btn btn-default" type="button" id="mainbt">APK Here</button></div>
+<div id="myModal3" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">APK Here<b id="lit"> (www.apkhere.com)</b></h4>
+				<hr>
+		<table class="table table-responsive">
+	<thead>
+		<tr>
+			<th>
+				Ads
+			</th>
+			<th>
+				Captcha
+			</th>
+			<th>
+				Steps
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+				Few
+			</td>
+			<td>
+				Yes (easy)
+			</td>
+			<td>
+				2
+			</td>
+		</tr>
+	</tbody>
+</table>
+<!-- Rank -->
+
+  <button type="button" class="btn btn-info" id="bttn" data-toggle="collapse" data-target="#demo3">Alexa Rank</button>
+  <div id="demo3" class="collapse">
+    <div id="rank"><a href="http://www.alexa.com/siteinfo/apkhere.com"><script type="text/javascript" src="http://xslt.alexa.com/site_stats/js/t/a?url=apkhere.com"></script></a></div>
+  </div>
+
+<!-- Rank -->
+      </div>
+      <div class="modal-body">
+      	<?php  
+    $action_path3 = "http://www.apkhere.com";
+    ?>
+	    <script type="text/javascript">
+        function submitData3() {
+            var a = null;
+            var _C = document.getElementById("_C").value;
+
+            a = _C;
+
+            window.location.href = "<?php echo $action_path3; ?>/search?q=" + a;
+
+            return;
+        }
+    </script>
+    <h4 id="tiitle">e.g. whatsapp</h4>
+<input type="text" name="apkhere" id="_C" class="form-control input-lg" placeholder="app name" required>
+<input type="submit" name="apkhere" onclick="submitData3();" value="Search" class="btn btn-default" id="btt2">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<!-- end -->
+
+<!-- start Apps APK -->
+	<div id="sitee">
+<div id="btt"><button data-target="#myModal4" data-toggle="modal" class="btn btn-default" type="button" id="mainbt">Apps APK</button></div>
+<div id="myModal4" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Apps APK<b id="lit"> (www.appsapk.com)</b></h4>
+				<hr>
+		<table class="table table-responsive">
+	<thead>
+		<tr>
+			<th>
+				Ads
+			</th>
+			<th>
+				Captcha
+			</th>
+			<th>
+				Steps
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+				Few
+			</td>
+			<td>
+				No
+			</td>
+			<td>
+				1
+			</td>
+		</tr>
+	</tbody>
+</table>
+<!-- Rank -->
+
+  <button type="button" class="btn btn-info" id="bttn" data-toggle="collapse" data-target="#demo4">Alexa Rank</button>
+  <div id="demo4" class="collapse">
+    <div id="rank"><a href="http://www.alexa.com/siteinfo/appsapk.com"><script type="text/javascript" src="http://xslt.alexa.com/site_stats/js/t/a?url=appsapk.com"></script></a></div>
+  </div>
+
+<!-- Rank -->
+      </div>
+      <div class="modal-body">
+      	<?php
+    $action_path4 = "http://www.appsapk.com/";
+    ?>
+	    <script type="text/javascript">
+        function submitData4() {
+            var a = null;
+            var _D = document.getElementById("_D").value;
+
+            a = _D;
+
+            window.location.href = "<?php echo $action_path4; ?>?s=" + a;
+
+            return;
+        }
+    </script>
+    <h4 id="tiitle">e.g. whatsapp</h4>
+<input type="text" name="appsapk" id="_D" class="form-control input-lg" placeholder="app name" required>
+<input type="submit" name="appsapk" onclick="submitData4();" value="Search" class="btn btn-default" id="btt2">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<!-- end -->
+
+<!-- start APK Pure -->
+	<div id="sitee">
+<div id="btt"><button data-target="#myModal5" data-toggle="modal" class="btn btn-default" type="button" id="mainbt">APK Pure</button></div>
+<div id="myModal5" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">APK Pure<b id="lit"> (www.apkpure.com)</b></h4>
+				<hr>
+		<table class="table table-responsive">
+	<thead>
+		<tr>
+			<th>
+				Ads
+			</th>
+			<th>
+				Captcha
+			</th>
+			<th>
+				Steps
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+				Few
+			</td>
+			<td>
+				No
+			</td>
+			<td>
+				1
+			</td>
+		</tr>
+	</tbody>
+</table>
+<!-- Rank -->
+
+  <button type="button" class="btn btn-info" id="bttn" data-toggle="collapse" data-target="#demo5">Alexa Rank</button>
+  <div id="demo5" class="collapse">
+    <div id="rank"><a href="http://www.alexa.com/siteinfo/apkpure.com"><script type="text/javascript" src="http://xslt.alexa.com/site_stats/js/t/a?url=apkpure.com"></script></a></div>
+  </div>
+
+<!-- Rank -->
+      </div>
+      <div class="modal-body">
+      	<?php  
+    $action_path5 = "http://apkpure.com";
+    ?>
+	    <script type="text/javascript">
+        function submitData5() {
+            var a = null;
+            var _E = document.getElementById("_E").value;
+
+            a = _E;
+
+            window.location.href = "<?php echo $action_path5; ?>/search?q=" + a;
+
+            return;
+        }
+    </script>
+    <h4 id="tiitle">e.g. whatsapp</h4>
+<input type="text" name="apkpure" id="_E" class="form-control input-lg" placeholder="app name" required>
+<input type="submit" name="apkpure" onclick="submitData5();" value="Search" class="btn btn-default" id="btt2">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<!-- end -->
+
+<!-- start APK-DL -->
+	<div id="sitee">
+<div id="btt"><button data-target="#myModal6" data-toggle="modal" class="btn btn-default" type="button" id="mainbt">APK-DL</button></div>
+<div id="myModal6" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">APK-DL<b id="lit"> (www.apk-dl.com)</b></h4>
+				<hr>
+		<table class="table table-responsive">
+	<thead>
+		<tr>
+			<th>
+				Ads
+			</th>
+			<th>
+				Captcha
+			</th>
+			<th>
+				Steps
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+				Few
+			</td>
+			<td>
+				No
+			</td>
+			<td>
+				1
+			</td>
+		</tr>
+	</tbody>
+</table>
+<!-- Rank -->
+
+  <button type="button" class="btn btn-info" id="bttn" data-toggle="collapse" data-target="#demo6">Alexa Rank</button>
+  <div id="demo6" class="collapse">
+    <div id="rank"><a href="http://www.alexa.com/siteinfo/apk-dl.com"><script type="text/javascript" src="http://xslt.alexa.com/site_stats/js/t/a?url=apk-dl.com"></script></a></div>
+  </div>
+
+<!-- Rank -->
+      </div>
+      <div class="modal-body">
+      	<?php  
+    $action_path6 = "http://apk-dl.com";
+    ?>
+	    <script type="text/javascript">
+        function submitData6() {
+            var a = null;
+            var _F = document.getElementById("_F").value;
+
+            a = _F;
+
+            window.location.href = "<?php echo $action_path6; ?>/search?q=" + a;
+
+            return;
+        }
+    </script>
+    <h4 id="tiitle">e.g. whatsapp</h4>
+<input type="text" name="apkdl" id="_F" class="form-control input-lg" placeholder="app name" required>
+<input type="submit" name="apkdl" onclick="submitData6();" value="Search" class="btn btn-default" id="btt2">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<!-- end -->
+
+<!-- start APK Dot -->
+	<div id="sitee">
+<div id="btt"><button data-target="#myModal7" data-toggle="modal" class="btn btn-default" type="button" id="mainbt">APK Dot</button></div>
+<div id="myModal7" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">APK Dot<b id="lit"> (www.apkdot.com)</b></h4>
+				<hr>
+		<table class="table table-responsive">
+	<thead>
+		<tr>
+			<th>
+				Ads
+			</th>
+			<th>
+				Captcha
+			</th>
+			<th>
+				Steps
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+				Few
+			</td>
+			<td>
+				No
+			</td>
+			<td>
+				1
+			</td>
+		</tr>
+	</tbody>
+</table>
+<!-- Rank -->
+
+  <button type="button" class="btn btn-info" id="bttn" data-toggle="collapse" data-target="#demo7">Alexa Rank</button>
+  <div id="demo7" class="collapse">
+    <div id="rank"><a href="http://www.alexa.com/siteinfo/apkdot.com"><script type="text/javascript" src="http://xslt.alexa.com/site_stats/js/t/a?url=apkdot.com"></script></a></div>
+  </div>
+
+<!-- Rank -->
+      </div>
+      <div class="modal-body">
+      	<?php  
+    $action_path7 = "http://apkdot.com/";
+    ?>
+	    <script type="text/javascript">
+        function submitData7() {
+            var a = null;
+            var _J = document.getElementById("_J").value;
+
+            a = _J;
+
+            window.location.href = "<?php echo $action_path7; ?>?s=" + a;
+
+            return;
+        }
+    </script>
+    <h4 id="tiitle">e.g. whatsapp</h4>
+<input type="text" name="apkdot" id="_J" class="form-control input-lg" placeholder="app name" required>
+<input type="submit" name="apkdot" onclick="submitData7();" value="Search" class="btn btn-default" id="btt2">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<!-- end -->
+
+<!-- start APK Mirror -->
+	<div id="sitee">
+<div id="btt"><button data-target="#myModal8" data-toggle="modal" class="btn btn-default" type="button" id="mainbt">APK Mirror</button></div>
+<div id="myModal8" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">APK Mirror<b id="lit"> (www.apkmirror.com)</b></h4>
+				<hr>
+		<table class="table table-responsive">
+	<thead>
+		<tr>
+			<th>
+				Ads
+			</th>
+			<th>
+				Captcha
+			</th>
+			<th>
+				Steps
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+				Few
+			</td>
+			<td>
+				No
+			</td>
+			<td>
+				1
+			</td>
+		</tr>
+	</tbody>
+</table>
+<!-- Rank -->
+
+  <button type="button" class="btn btn-info" id="bttn" data-toggle="collapse" data-target="#demo8">Alexa Rank</button>
+  <div id="demo8" class="collapse">
+    <div id="rank"><a href="http://www.alexa.com/siteinfo/apkmirror.com"><script type="text/javascript" src="http://xslt.alexa.com/site_stats/js/t/a?url=apkmirror.com"></script></a></div>
+  </div>
+
+<!-- Rank -->
+      </div>
+      <div class="modal-body">
+      	<?php  
+    $action_path8 = "http://www.apkmirror.com/";
+    ?>
+	    <script type="text/javascript">
+        function submitData8() {
+            var a = null;
+            var _H = document.getElementById("_H").value;
+
+            a = _H;
+
+            window.location.href = "<?php echo $action_path8; ?>?s=" + a;
+
+            return;
+        }
+    </script>
+    <h4 id="tiitle">e.g. whatsapp</h4>
+<input type="text" name="apkmirror" id="_H" class="form-control input-lg" placeholder="app name" required>
+<input type="submit" name="apkmirror" onclick="submitData8();" value="Search" class="btn btn-default" id="btt2">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<!-- end -->
+</div>
+<!-- footer -->
+<div id="footer"><div id="ttt">By Mohammad Atwi | 2016. All rights reserved.</div></div>
+<script>
+$('#loginModal').on('show.bs.modal', function (e) {
+     $('body').addClass('test');
+});
+</script>
+</body>
+</html>
